0d40b6893e2375ee06daf1f2f80a751d24826df6
Miskalicious <aaronzimmermann@gmail.com>
2017-02-14 15:42:28 -0500

    Update brewget.sh

--- dotfiles/brewget.sh
+++ dotfiles/brewget.sh
@@ -3,3 +3,7 @@
 # install homebrew package manager
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# set up to brew
+brew doctor
+# turn analytics off
+brew analytics off
